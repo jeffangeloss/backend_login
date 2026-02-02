5a5ce993f11ffaf9586914660e0722c6d4324c3e
L33103 <L33103@lab.aca>
2026-02-02 15:13:17 -0500

    Backend Login con 5 - 8 caracteres

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class LoginRequest(BaseModel):
+    username : str = Field(..., min_length=5)
+    password : str = Field(..., min_length=8)
+
+@app.post("/login")
+async def login(login_request : LoginRequest):
+    if login_request.username == "PROGRAWEB" and login_request.password == "123123123":
+        # Si pones la contraseña correspondiente te mostrará el siguiente mensaje
+        return {
+            "msg" : "Acceso concedido"
+        }
+    else:
+        raise HTTPException(
+            status_code=400, 
+            detail="Error en el login, credenciales incorrectas")
+        # return{
+        #     "msg" : "Error en login"
+        # }
